4a25cfa1cde91fd439b6d858315a44fc4f5f104d
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-11-21 15:50:22 -0700

    update dynamic routes for post ids need to extract post id's from route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-//import axios from 'axios';
-import axios from '../../axios';
 import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
@@ -32,7 +31,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
+        <Route path="/new-post" exact component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import { Link } from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -39,12 +40,13 @@ class Posts extends Component {
       posts = this.state.posts.map(
         post => {
           return (
-            <Post
-              key={post.id}
+            <Link to={'/' + post.id} key={post.id}>
+              <Post
               title={post.title}
               author={post.author}
               clicked={() => this.postSelectedHandler(post.id)}
-            />
+              />
+          </Link>
           );
         }
       );
